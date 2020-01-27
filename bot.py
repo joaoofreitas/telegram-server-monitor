b8b1c9c600e46f4b7db1f3d44304b0ca9ab822e6
Иван <31630346+joaoofreitas@users.noreply.github.com>
2020-01-27 00:25:28 +0000

    Public IP Address Fetching and Server Sensors information

--- bot.py
+++ bot.py
@@ -1,8 +1,37 @@
-import telegram
+from subprocess import check_output as term
+from requests import get
+import telebot
+
 
 TOKEN = ""
 bot = telebot.TeleBot(TOKEN)
 
+ip = get('https://api.ipify.org').text
+
+
+def terminal(command):
+    output = term([command])
+    return output
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Hello, Im Anton how can I help you?")
+	bot.reply_to(message, "Hello, Im Anton. How can I help you?")
+
+
+@bot.message_handler(commands=['showip', 'help'])
+def showIP(message):
+	bot.reply_to(message, "My IP Address is: " + ip)
+
+
+@bot.message_handler(commands=['sensors', 'help'])
+def sensors(message):
+	bot.reply_to(message, terminal('/opt/vc/bin/vcgencmd measure_temp'))
+
+bot.polling()
+
+
+'''
+
+showip - Show my IP
+sensors - How do I feel right now
+'''
