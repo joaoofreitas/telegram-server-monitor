0392ab650b70e7fc61568c1d02aa0a46f9fcd2f7
joaoofreitas <jota.e.freitas@gmail.com>
2020-02-02 21:58:41 +0000

    chatid-scapper feature added... on progress

---
+++ chatid-scrapper.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+import telebot
+import subprocess
+from requests import get
+from json import loads as jsonloads
+
+def get_folder():
+    folder = subprocess.run('pwd', shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+    folder = folder.stdout
+    return folder[:0]
+
+class json_Update:
+    def CHAT_ID_fetcher(self):
+        try:    
+            with open(get_folder() + 'getUpdates.json', 'r') as updatesFile:
+                updates = updatesFile.read()
+            updateFile = jsonloads(updates)
+            output = updateFile["result"][0]["message"]["from"]["id"]
+            return str(output)
+
+        except:
+            print("ERROR IMPORTING the getUpdates.json file for reading, please check your absolute path or folder permissions")
+
+
+    def writeUpdates(self,FETCH):
+        try:    
+            with open(get_folder() + 'getUpdates.json', 'w+') as updatesFile:
+                updatesFile.write(FETCH)
+
+        except:
+            print("ERROR IMPORTING the getUpdates.json file, for writing, please check your absolute path or folder permissions")
+
+
+try:
+    with open(get_folder() + 'config.json', 'r') as configFile:
+        config = configFile.read()
+    file = jsonloads(config)
+
+    TOKEN = file["TOKEN"]
+    CHAT_ID = file["CHAT_ID"]
+    print("Configuration File Imported Sucessfully")
+
+except:
+    print("ERROR IMPORTING the config.json file, please check your absolute path or folder permissions")
+
+URL = 'https://api.telegram.org/bot' + TOKEN +'/getUpdates'
+FETCH = get(URL).text
+
+getUpdates = json_Update()
+getUpdates.writeUpdates(FETCH)
+CHAT_ID = getUpdates.CHAT_ID_fetcher()
